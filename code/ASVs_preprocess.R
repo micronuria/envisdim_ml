94a011ba1ffd14209317884eaaf3875fec793f4f
micronuria <nuria.fgonzalez@gmail.com>
2023-05-08 18:50:06 +0200

    preprocess counts and metadata, group by genus and chlorophill groups added

--- code/ASVs_preprocess.R
+++ code/ASVs_preprocess.R
@@ -7,17 +7,49 @@ library(tidyverse)
 
 
 # load ASVs count raw data
-read_tsv("raw_data/asvs_raw_counts.tsv", 
+all_asv <- read_tsv("raw_data/asvs_raw_counts.tsv", 
          col_types = cols(sample = col_character(),
-                          .default = col_integer())) 
-
-%>% 
-  pivot_longer()
+                          .default = col_integer())) %>% 
+  pivot_longer(-sample, names_to = "asv", values_to = "count")
 
 
+# GROUPED DATA
 
-### create a dataframe with taxonomy
+# load taxonomy data up to Genus level only
+tax_raw <- read_tsv("raw_data/asvs_taxonomy.tsv") %>%
+  select(asv, Kingdom, Phylum, Class, Order, Family, Genus) 
 
+
+# Group taxonomy to filter data
+taxonomy <- tax_raw %>%
+  unite("taxonomy", Kingdom:Genus, sep = ";", remove = TRUE) %>%
+  mutate(taxonomy = str_replace(taxonomy, ";NA","_unclassified"),
+         taxonomy = str_replace_all(taxonomy, ";NA", ""),
+         taxonomy = str_replace_all(taxonomy, ".*;", ""))
+
+# read metadata
+chlclass <- read_tsv("raw_data/chlclass.tsv") 
+
+# check number of cases on bloom and no-bloom categories
+chlclass %>% count(event)
+ 
+
+# join count and taxonomy and ...
+composite <- inner_join(all_asv, taxonomy, by = "asv") %>%
+  group_by(sample, taxonomy) %>%                        # group by genus
+  summarize(count = sum(count), .groups="drop") %>% 
+  group_by(sample) %>%                                  # calculate relative abundances
+  mutate(rel_abun = count / sum(count)) %>% 
+  ungroup() %>%                                         # get rid of grouping structure on data
+  select(-count) %>%                                    # get rid of count column
+  inner_join(., chlclass, by="sample")                  # add data about chlorophill groups
+
+##### add metadata!!!
+
+
+#####################################
+# REMOVE eUK ANd ALL NA - chloroplasts
+###################################
 
 # add taxonomy columns
 asvs_raw_t <- cbind(taxa.sp.print, asvs_raw)
@@ -66,4 +98,7 @@ asvs_test <- t(asvs_test.w[,c(175,8:174)])
 chlclass <- read.table("/data/mcm/nfernandez/envisdim/analysis/intermediate/env_metadata/chlclass.tsv",
                        sep = "\t", header = TRUE)
 
+ \\(\  MP)(\d+)
 
+
+print $1 $2
